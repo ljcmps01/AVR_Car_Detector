d9c919599c9140e5fe6d8f60930d870e396aad35
Alejo Rodrigo Campos <46141665+ljcmps01@users.noreply.github.com>
2024-06-18 09:28:22 -0300

    W5100 mqtt driver (#7)
    * firware(W5100): first W5100 library set up
    
    Working library to control the W5100 based Ethernet module
    with a driver to simplify the main file with AVR registers
    
    Notes: code cleanup and clarity is needed overall
    ASAP
    
    files: external/W5100_ATMega_WEBSERV/, src/drivers/avr_5100*
    
    * build (w5100): added extra commit to add w5100 lib
    
    Trying to fix missing files issue with an
    intermediate commit which downloads and the w5100
    repo properly
    
    files: (external/w5100)
    
    * build(avr_w5100): comment out avr_w5100
    
    comment out the avr_w5100.c so it will let the
    container run fine and update de w5100 files properly
    
    * build(avr_w5100): comment out avr_w5100
    
    Almost fully comment out the avr_w5100.c so it will let the
    container run fine and update de w5100 files properly
    
    * build (makefile) commented /external refs
    
    * build(Makefile) add w5100 lib ref for compiling
    
    uncomment w5100 refs from the makefile to
    compile it and check if the submodule was added to
    the repo
    
    files: Makefile
    
    * Update ci.yml
    
    Run ls before compiling to check and debug if filesystem is properly set up
    
    * Update ci.yml
    
    * Update ci.yml
    
    * build(dockerfile) update dockerfile
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Firmware (w5100) basic working library for w5100
    
    Functional w5100 functions and test code to run
    a webserver using the w5100 shield
    
    files: avr_w5100.*,test/test_web_server.c

--- src/drivers/avr_w5100.h
+++ src/drivers/avr_w5100.h
@@ -1,16 +1,20 @@
-#ifndef  AVR_W5100_H
-#define  AVR_W5100_H
 
+#ifndef AVR_W5100_H
+#define AVR_W5100_H
 
+#include "../../external/w5100/w5100.h"
 
 void spi_init();
 
-unsigned char OpenSocket(unsigned char sock, unsigned char eth_protocol, unsigned int tcp_port);
+unsigned char OpenSocket(unsigned char sock, unsigned char eth_protocol,
+                         unsigned int tcp_port);
 void CloseSocket(unsigned char sock);
 void DisconnectSocket(unsigned char sock);
 unsigned char Listen(unsigned char sock);
-unsigned char Send(unsigned char sock, const unsigned char *buf, unsigned int buflen);
-unsigned int Receive(unsigned char sock, unsigned char *buf, unsigned int buflen);
+unsigned char Send(unsigned char sock, const unsigned char *buf,
+                   unsigned int buflen);
+unsigned int Receive(unsigned char sock, unsigned char *buf,
+                     unsigned int buflen);
 unsigned int ReceivedSize(unsigned char sock);
 
 void my_select(void);
@@ -20,12 +24,8 @@ void my_reset(void);
 
 void W5100_set_callbacks();
 
-unsigned char W5100_init(\
-    unsigned char* MAC,\
-    unsigned char* IP,\
-    unsigned char* SB,\
-    unsigned char* GW\
-);
+unsigned char W5100_init(unsigned char *MAC, unsigned char *IP,
+                         unsigned char *SB, unsigned char *GW);
 
 #define MAX_BUF 512 /* largest buffer we can read from chip */
 
